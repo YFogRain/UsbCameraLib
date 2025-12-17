a2931a77ea9ebf0a045dda38eb71cc60cbadfcdc
rain <15921316892@163.com>
2025-12-17 13:23:12 +0800

    1. 修改超时时长

--- libusbcamera/src/main/cpp/libusb/io.c
+++ libusbcamera/src/main/cpp/libusb/io.c
@@ -2431,8 +2431,8 @@ int API_EXPORTED libusb_handle_events_completed(libusb_context *ctx,
 	int *completed)
 {
 	struct timeval tv;
-	tv.tv_sec = 60;
-	tv.tv_usec = 0;
+	tv.tv_sec = 0;
+	tv.tv_usec = 500000;
 	return libusb_handle_events_timeout_completed(ctx, &tv, completed);
 }
 
